86824aadceeceeeb91c6ba4e1f353f7a16c580e8
HP <HP@DESKTOP-M8RIVEK>
2019-04-13 17:01:37 +0530

    gf

---
+++ Project/flip1/src/main/SysGetProperty.java
@@ -0,0 +1,22 @@
+package main;
+
+public class SysGetProperty {
+
+	public static void main(String[] args) {
+
+System.out.println("muthu");
+
+System.out.println(System.getProperty("user.dir"));
+
+System.out.println(System.getProperty("os.name"));
+
+System.out.println(System.getProperty("java.runtime.version" ));
+System.out.println(System.getProperty("user.name"));   
+System.out.println(System.getProperty("user.home"));
+
+System.out.println(System.getProperty("release"));
+System.out.println(System.getProperty("wall"));
+
+	}
+
+}
